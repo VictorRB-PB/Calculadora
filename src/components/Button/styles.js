d281f86c08d97070b0c7fdf7c5b656f0484a6553
VictorRB <victorbraz_@hotmail.com>
2023-11-23 17:12:20 -0300

    primeiro commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCDCD;
+    background-color: #00AAFD;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-meight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
